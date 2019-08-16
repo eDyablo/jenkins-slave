9b84896de5bcdfbc7407843ef8a0de7640513520
eduardi-earnin <eduard.iablonskyi@earnin.com>
2019-08-16 17:49:36 +0300

    update build script

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ set -o errexit
 set -o pipefail
 
 image_name="${IMAGE_NAME:-"jnlp-slave"}"
-image_version="${IMAGE_VERSION:-}"
+image_version="${IMAGE_VERSION:-$(cat version)}"
 image_repository="${IMAGE_REPOSITORY:-"addword"}"
-image_tag="${IMAGE_TAG:-"${image_repository}"/"${image_name}""${image_version:+":${image_version}"}"}"
+image_build_tag="${IMAGE_TAG:-"${image_repository}"/"${image_name}"}"
+image_tag="${IMAGE_TAG:-"${image_build_tag}""${image_version:+":${image_version}"}"}"
 docker_context="."
 docker_file="Dockerfile"
 
-docker build --tag="${image_tag}" --file="${docker_file}" "${docker_context}"
+docker build --tag="${image_build_tag}" --file="${docker_file}" "${docker_context}"
+docker tag ${image_build_tag} ${image_tag}
